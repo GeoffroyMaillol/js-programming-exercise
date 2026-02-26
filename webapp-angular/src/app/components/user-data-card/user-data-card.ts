a4f53bab875dde2831bc34905b6333f561a10327
Geoffroy Maillol <geoffroy.maillol@gmail.com>
2026-02-26 10:38:58 +0000

    Added animation stagger

--- webapp-angular/src/app/components/user-data-card/user-data-card.ts
+++ webapp-angular/src/app/components/user-data-card/user-data-card.ts
@@ -10,13 +10,13 @@ import { UserData } from '../../types/UserData';
 })
 export class UserDataCard {
   @Input({ required: true }) userData!: UserData;
+  @Input({ required: true }) delay: number = 0;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnChanges() {
+    this.el.nativeElement.style.animationDelay = `${this.delay}ms`;
     this.renderer.removeClass(this.el.nativeElement, 'animate');
-    void this.el.nativeElement.offsetWidth; // force reflow
     this.renderer.addClass(this.el.nativeElement, 'animate'); // add class again to trigger animation
   }
-
 }
